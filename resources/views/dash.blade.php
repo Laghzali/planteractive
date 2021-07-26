c8d5770b075b9d6861ef5ed658c2ff77a613efd1
laghzali <mohamed.laghzali@uit.ac.ma>
2021-07-26 01:33:25 +0000

    return loadmap

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -503,7 +503,9 @@ jQuery( document ).ready(function() {
                             li = document.createElement('li')
                             li.id = "map"+data[elm].id
                             li.innerHTML += '<a class="dropdown-item" href="#">'+data[elm].name+'</a>'
-                            li.onclick "loadMap("+data[elm].path +" ," + data[elm].id + ")"
+                            li.onclick = function () {
+                                return loadMap(data[elm].path , data[elm].id)
+                            }
                             ul.appendChild(li)
                             
                         }
