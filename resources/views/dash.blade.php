7420cfae6fa37a8f1b834e91a0f268169586772f
laghzali <mohamed.laghzali@uit.ac.ma>
2021-07-26 01:40:43 +0000

    arriow

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -503,7 +503,7 @@ jQuery( document ).ready(function() {
                             li = document.createElement('li')
                             li.id = "map"+data[elm].id
                             li.innerHTML += '<a class="dropdown-item" href="#">'+data[elm].name+'</a>'
-                            li.setAttribute('onclick' , function() {
+                            li.setAttribute('onclick' , () => {
                               return "loadMap("+data[elm].path+", "+data[elm].id+")"})
 
                             ul.appendChild(li)
