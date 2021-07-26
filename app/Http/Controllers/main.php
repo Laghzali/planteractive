f9f308d185a5655a2f34cf8988bfc5d937fa8ad7
laghzali <mohamed.laghzali@uit.ac.ma>
2021-07-26 02:29:57 +0000

    switched overlays to maps

--- app/Http/Controllers/main.php
+++ app/Http/Controllers/main.php
@@ -65,8 +65,8 @@ class main extends Controller
     }
     public function retriveOverlay(Request $request) {
     //$overlays = overlays::get()->toJson(JSON_PRETTY_PRINT);
-    $overlays = overlays::selectRaw('overlays.id as overlay_id , maps.id as map_overlay_id, overlays.name , overlays.x , overlays.y , overlays.symbol , overlays.image , overlays.note , overlays.color')
-        ->join('maps' , 'maps.id' , '='  , 'overlays.map_id')->where('map_overlay_id' , $request->map_id)->get()->toJson(JSON_PRETTY_PRINT);
+    $overlays = maps::selectRaw('overlays.id as overlay_id , maps.id as map_overlay_id, overlays.name , overlays.x , overlays.y , overlays.symbol , overlays.image , overlays.note , overlays.color')
+        ->join('overlays' , 'maps.id' , '='  , 'overlays.map_id')->where('map_overlay_id' , $request->map_id)->get()->toJson(JSON_PRETTY_PRINT);
     return response($overlays, 200);
 
     }
